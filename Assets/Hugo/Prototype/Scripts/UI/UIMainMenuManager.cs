97e9642b4e60c1708f795eb21c5f4277a5c92510
Neiflheim <isambart.hugo0912@gmail.com>
2024-12-08 14:08:19 +0100

    Add_CursorDisable

--- Assets/Hugo/Prototype/Scripts/UI/UIMainMenuManager.cs
+++ Assets/Hugo/Prototype/Scripts/UI/UIMainMenuManager.cs
@@ -5,6 +5,12 @@ namespace Hugo.Prototype.Scripts.UI
 {
     public class UIMainMenuManager : MonoBehaviour
     {
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
         public void ChangeScene(int index)
         {
             SceneManager.LoadScene(index);
