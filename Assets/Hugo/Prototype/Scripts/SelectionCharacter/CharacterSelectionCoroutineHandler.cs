d836497f6b3dcdf200605691142dbd8f53fb7f91
Neiflheim <isambart.hugo0912@gmail.com>
2024-12-10 16:12:08 +0100

    Add_DisplayTimerSelectionBlop

--- Assets/Hugo/Prototype/Scripts/SelectionCharacter/CharacterSelectionCoroutineHandler.cs
+++ Assets/Hugo/Prototype/Scripts/SelectionCharacter/CharacterSelectionCoroutineHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Hugo.Prototype.Scripts.Game;
 using Hugo.Prototype.Scripts.Utils;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,22 +12,41 @@ namespace Hugo.Prototype.Scripts.SelectionCharacter
         // Coroutine
         private Coroutine _loadSceneCoroutine;
 
+        [Header("Text Timer")]
+        [SerializeField] private GameObject _panelTimer;
+        [SerializeField] private TextMeshProUGUI _textTimer;
+
         private void Update()
         {
             if (GameManager.FirstPlayerScriptableObject && GameManager.SecondPlayerScriptableObject && !GameManager.HasGameLoaded)
             {
                 GameManager.HasGameLoaded = true;
+                _panelTimer.SetActive(true);
                 _loadSceneCoroutine = StartCoroutine(LoadSceneWithDelay(3f));
             }
 
             if (!GameManager.FirstPlayerScriptableObject || !GameManager.SecondPlayerScriptableObject)
             {
+                _panelTimer.SetActive(false);
                 CancelLoadScene();
             }
         }
 
         private IEnumerator LoadSceneWithDelay(float delay)
         {
+            float remainingTime = delay;
+
+            while (remainingTime > 0)
+            {
+                _textTimer.text = Mathf.Ceil(remainingTime).ToString();
+                
+                yield return null;
+                
+                remainingTime -= Time.deltaTime;
+            }
+
+            _textTimer.text = " Go ! ";
+            
             yield return new WaitForSeconds(delay);
 
             SceneManager.LoadScene(2);
