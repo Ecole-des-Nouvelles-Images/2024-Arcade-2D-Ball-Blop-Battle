40503311c7dd09fafdfcad4218f05b28fb395e24
Neiflheim <isambart.hugo0912@gmail.com>
2024-12-19 16:53:34 +0100

    Modify_SplashScreen

--- Assets/Hugo/Prototype/Scripts/SlpashScreen/SplashScreenHandler.cs
+++ Assets/Hugo/Prototype/Scripts/SlpashScreen/SplashScreenHandler.cs
@@ -11,8 +11,9 @@ namespace Hugo.Prototype.Scripts.SlpashScreen
         [Header("Settings")]
         [SerializeField] private float _fadeInDuration;
         [SerializeField] private float _fadeOutDuration;
-        [SerializeField] private float _screenStay;
-        [SerializeField] private float _lauchGame;
+        [SerializeField] private float _timeScreenEnsiStay;
+        [SerializeField] private float _timeScreenLoadingStay;
+        [SerializeField] private float _intervalLoadingPoint;
         
         [Header("References")]
         [SerializeField] private Image _screenEnsi;
@@ -20,13 +21,26 @@ namespace Hugo.Prototype.Scripts.SlpashScreen
         [SerializeField] private TextMeshProUGUI _textChargement;
         [SerializeField] private SpriteRenderer _animationChargement;
 
+        private int _currentDots = 0;
+        private float _timer;
+
         private void Start()
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
             
             _screenEnsi.DOFade(1f, _fadeInDuration);
-            Invoke(nameof(FadeOutEnsi), _screenStay);
+            Invoke(nameof(FadeOutEnsi), _timeScreenEnsiStay);
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= _intervalLoadingPoint)
+            {
+                _timer = 0f;
+                DisplayLoandingPoints();
+            }
         }
 
         private void FadeOutEnsi()
@@ -41,12 +55,32 @@ namespace Hugo.Prototype.Scripts.SlpashScreen
             _animationChargement.DOFade(1f, _fadeInDuration);
             _illustration.DOFade(1f, _fadeInDuration);
             _textChargement.DOFade(1f, _fadeInDuration);
-            Invoke(nameof(ChangeScene), _lauchGame);
+            Invoke(nameof(ChangeScene), _timeScreenLoadingStay);
         }
 
         private void ChangeScene()
         {
             SceneManager.LoadScene(1);
+        }
+
+        private void DisplayLoandingPoints()
+        {
+            _currentDots = (_currentDots + 1) % 4; // Boucle entre 0, 1, 2, 3
+            switch (_currentDots)
+            {
+                case 0:
+                    _textChargement.text = "Chargement";
+                    break;
+                case 1:
+                    _textChargement.text = "Chargement.";
+                    break;
+                case 2:
+                    _textChargement.text = "Chargement..";
+                    break;
+                case 3:
+                    _textChargement.text = "Chargement...";
+                    break;
+            }
         }
     }
 }
